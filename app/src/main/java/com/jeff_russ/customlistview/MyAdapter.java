3e969bb2b4570b19a767d68f7948936f3da8007d
Jeff-Russ <jeffreylynnruss@gmail.com>
2015-09-19 18:51:44 -0400

    added comments

--- app/src/main/java/com/jeff_russ/customlistview/MyAdapter.java
+++ app/src/main/java/com/jeff_russ/customlistview/MyAdapter.java
@@ -8,21 +8,46 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+// We can create custom adapters
 class MyAdapter extends ArrayAdapter<String> {
 
-    public MyAdapter(Context context, String[] values) {
+    public MyAdapter(Context context, String[] values){
+
         super(context, R.layout.row_layout_2, values);
+
     }
 
+    // Override getView which is responsible for creating the rows for our list
+    // position represents the index we are in for the array.
+
+    // convertView is a reference to the previous view that is available for reuse. As
+    // the user scrolls the information is populated as needed to conserve memory.
+
+    // A ViewGroup are invisible containers that hold a bunch of views and
+    // define their layout properties.
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater theInflator = LayoutInflater.from(getContext());
-        View theView = theInflator.inflate(R.layout.row_layout_2, parent, false);
-        String tvShow = getItem(position);
+
+        // The LayoutInflator puts a layout into the right View
+        LayoutInflater theInflater = LayoutInflater.from(getContext());
+
+        // inflate takes the resource to load, the parent that the resource may be
+        // loaded into and true or false if we are loading into a parent view.
+        View theView = theInflater.inflate(R.layout.row_layout_2, parent, false);
+
+        String tvShow = getItem(position);  // We retrieve the text from the array
+
+        // Get the TextView we want to edit
         TextView theTextView = (TextView) theView.findViewById(R.id.textView1);
-        theTextView.setText(tvShow);
+
+        theTextView.setText(tvShow);    // Put the next TV Show into the TextView
+
+        // Get the ImageView in the layout
         ImageView theImageView = (ImageView) theView.findViewById(R.id.imageView1);
-        theImageView.setImageResource(R.drawable.circle);
+
+        theImageView.setImageResource(R.drawable.circle); // We can set a ImageView like this
+
         return theView;
+
     }
 }
